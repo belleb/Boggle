c70450cc52e6ddf6f65026b75501e11bb94a506d
belleb <isabel.leal.il@gmail.com>
2019-01-27 23:59:42 -0500

    server.cpp and templates modified

--- server.cpp
+++ server.cpp
@@ -4,6 +4,7 @@
 int main()
 {
     crow::SimpleApp app;
+    crow::mustache::set_base(".");
 
     CROW_ROUTE(app, "/")([](){
         pair <string, string> board;
@@ -12,5 +13,22 @@ int main()
     });
     
     
+
+    CROW_ROUTE(app, "/game")([](){
+        srand (time(NULL));
+        pair <string, string> board;
+        board = new_board();
+        crow::mustache::context x;
+         
+        x["id"] = board.first;
+        x["letters"] = board.second;
+        
+        auto page = crow::mustache::load("/templates/index.html");
+        return page.render(x);
+    });
+    
+    
     app.port(18080).multithreaded().run();
 }
+
+// g++ server.cpp -o server -I /usr/include/postgresql/ -L /usr/lib/ -lm -lpq -pthread -lboost_system -std=c++11
