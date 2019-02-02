590e1a5109051c5548c14b8fd1e974b093a298c6
belleb <isabel.leal.il@gmail.com>
2019-02-02 15:11:29 -0500

    changed frequencies for letters in English

--- board.cpp
+++ board.cpp
@@ -6,13 +6,15 @@
 using namespace std;
 
 // frequencies of letters (English)
-char frequencies_en[153] = {'E','E','E','E','E','E','E','E','E','E','E','E','E','E','E','E','E','E','E',
-'T','T','T','T','T','T','T','T','T','T','T','T','T','A','A','A','A','A','A','A','A','A','A','A','A',
-'R','R','R','R','R','R','R','R','R','R','R','R','I','I','I','I','I','I','I','I','I','I','I','N','N','N',
-'N','N','N','N','N','N','N','N','O','O','O','O','O','O','O','O','O','O','O','S','S','S','S','S','S',
-'S','S','S','S','S','S','D','D','D','D','D','D','C','C','C','C','C','H','H','H','H','H','L','L','L','L','L',
-'F','F','F','F','M','M','M','M','P','P','P','P','U','U','U','U','G','G','G','Y','Y','Y','W','W',
-'B','J','K','Q','V','X','Z'};
+char frequencies_en[205] = {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 
+'E', 'E', 'E', 'E', 'E', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'A', 'A', 
+'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 
+'O', 'O', 'O', 'O', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'N', 'N', 'N', 'N', 'N', 
+'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'R', 'R', 'R', 
+'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'D', 'D', 'D', 'D', 
+'D', 'D', 'D', 'D', 'D', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'U', 'U', 'U', 'U', 'U', 'U', 'C', 'C', 'C', 'C', 'C', 'M', 
+'M', 'M', 'M', 'M', 'F', 'F', 'F', 'F', 'F', 'Y', 'Y', 'Y', 'Y', 'W', 'W', 'W', 'W', 'G', 'G', 'G', 'G', 'P', 'P', 'P', 'P', 
+'B', 'B', 'B', 'V', 'V', 'K', 'K', 'X', 'Q', 'J', 'Z'};
 
 // frequencies of letters (Portuguese) 
 char frequencies_pt[106] = {'A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','E','E','E','E','E','E','E','E',
@@ -25,7 +27,7 @@ char frequencies_pt[106] = {'A','A','A','A','A','A','A','A','A','A','A','A','A',
 char getLetter(string language){
     int letter_index;
     if (language == "en"){
-        letter_index = rand() % 153; //get index
+        letter_index = rand() % 205; //get index
         return frequencies_en[letter_index];
     }else if (language == "pt"){
         letter_index = rand() % 106; //get index
